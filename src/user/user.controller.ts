66d9de51403f36b4f6e644b980bc6f956dc6dda3
JadenKim-dev <jaden.kim5179@gmail.com>
2024-03-30 22:13:33 +0900

    upgrade to nestjs v10, change entity file name

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,6 @@
 import { Controller } from '@nestjs/common';
 import { Crud } from '@nestjs-library/crud';
-import { User } from './entity/user.entity';
+import { User } from './entity/User.entity';
 import { UserService } from './user.service';
 
 @Crud({ entity: User, logging: true })
