91edfa9fb9d99f6089821e0553a0d2addaeb7fe0
JadenKim-dev <jaden.kim5179@gmail.com>
2024-05-11 19:15:43 +0900

    feat: swagger config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('Nest Sample API')
+    .setDescription('API 문서입니다.')
+    .setVersion('0.0.1')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('/api-docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
